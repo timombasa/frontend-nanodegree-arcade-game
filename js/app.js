b6dcb12e12222952c86c03d1ed796b69456de7b2
timombasa <tim.ombasa@gmail.com>
2015-04-14 15:29:15 -0700

    Add constructors for Player and Enemy. Implement update() methods for both objects. Enemies moved across the screen.

--- js/app.js
+++ js/app.js
@@ -1,39 +1,74 @@
+var CANVAS_WIDTH = 505;
+var WIDTH = 60;
+
+
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+var Enemy = function(x,y) {
+    // The image/sprite for our enemies uses a helper to easily load images
     this.sprite = 'images/enemy-bug.png';
-}
+    this.x = x;
+    this.y = y;
+    //enemy speed.
+    // TODO: optimize it
+    this.speed = (Math.random() * (300-100)) + 150;
+};
 
-// Update the enemy's position, required method for game
+// Update the enemy's position
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    if (this.x > CANVAS_WIDTH + 100) {
+      this.x = -125;
+      this.y = this.y;
+    }
+    this.x += this.speed * dt;
+
+    this.top = this.y;
+    this.left = this.x;
+    this.bottom = this.y + WIDTH;
+    this.right = this.x + WIDTH;
 }
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
+var Player = function(x,y) {
+    console.trace('player instantiated!!');
+    this.sprite = 'images/char-boy.png';
+    this.x = x;
+    this.y = y;
+};
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+Player.prototype.update = function(dt) {
+    if (this.x > CANVAS_WIDTH + 100) {
+      this.x = 55;
+      this.y = 105;
+    }
+    this.x += this.speed * dt;
 
+    this.top = this.y;
+    this.left = this.x;
+    this.bottom = this.y + WIDTH;
+    this.right = this.x + WIDTH;
+}
 
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+// TODO
+Player.prototype.handleInput = function(keyPressed) {
+
+}
+
+var player = new Player(15, 45);
+var firstEnemy = new Enemy(-75, 60);
+var secondEnemy = new Enemy(-180, 145);
+var thirdEnemy = new Enemy(-105, 230);
+var allEnemies = [firstEnemy, secondEnemy, thirdEnemy];
 
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
